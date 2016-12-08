72b90efd123a5d5783a2c3d2be8d724b88a28a5b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-07 21:58:37 -0600

    add test

---
+++ src/nti/base/tests/test_mixins.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import assert_that
+from hamcrest import has_property
+does_not = is_not
+
+from nti.testing.matchers import validly_provides
+from nti.testing.matchers import verifiably_provides
+
+import time
+import unittest
+
+from nti.base.interfaces import ICreatedTime
+from nti.base.interfaces import ILastModified
+
+from nti.base.mixins import CreatedAndModifiedTimeMixin
+
+from nti.base.tests import SharedConfiguringTestLayer
+
+class TestMixins(unittest.TestCase):
+
+	layer = SharedConfiguringTestLayer
+
+	def test_plus_extend(self):
+		c = CreatedAndModifiedTimeMixin()
+		for iface in (ICreatedTime, ILastModified):
+			assert_that(c, validly_provides(iface))
+			assert_that(c, verifiably_provides(iface))
+
+		t = time.time() + 100
+		c.updateLastMod(t)
+		assert_that(c, has_property('lastModified', is_(t)))
+
+		c.updateLastModIfGreater(100)
+		assert_that(c, has_property('lastModified', is_(t)))
