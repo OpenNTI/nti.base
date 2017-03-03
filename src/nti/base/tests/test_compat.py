0aea3a0b5a5fee143f97095fe57728a6dd1d234b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-02 18:04:10 -0600

    add test for _compat. change encoding to UTF-8

---
+++ src/nti/base/tests/test_compat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import assert_that
+does_not = is_not
+
+import unittest
+
+from nti.base._compat import bytes_
+from nti.base._compat import unicode_
+
+from nti.base.tests import SharedConfiguringTestLayer
+
+
+class TestCompat(unittest.TestCase):
+
+    layer = SharedConfiguringTestLayer
+
+    def test_bytes(self):
+        assert_that(bytes_(u'\u2019'), is_(b'\xe2\x80\x99'))
+        assert_that(unicode_(b'\xe2\x80\x99'), is_(u'\u2019'))
