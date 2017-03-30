3677e24c203d3b9519db138bf69c7c4c1f14cc64
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-30 10:05:19 -0500

    add doc

--- src/nti/base/_compat.py
+++ src/nti/base/_compat.py
@@ -24,6 +24,10 @@ else:
     _unicode = unicode
 
 def bytes_(s, encoding='utf-8', errors='strict'):
+    """
+    If ``s`` is an instance of ``text_type``, return
+    ``s.encode(encoding, errors)``, otherwise return ``str(s)``
+    """
     if isinstance(s, text_type):
         return s.encode(encoding, errors)
     return s
