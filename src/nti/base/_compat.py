36a6f264ee1554781ac541ce18e69aeef637741b
csanchez <csanchez@192.168.1.7>
2017-04-07 21:08:30 -0500

    add py2 funcs

--- src/nti/base/_compat.py
+++ src/nti/base/_compat.py
@@ -10,7 +10,9 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import six
+import inspect
 
+PY2 = six.PY2
 PY3 = six.PY3
 
 text_type = six.text_type
@@ -27,11 +29,11 @@ else:
 def bytes_(s, encoding='utf-8', errors='strict'):
     """
     If ``s`` is an instance of ``text_type``, return
-    ``s.encode(encoding, errors)``, otherwise return ``str(s)``
+    ``s.encode(encoding, errors)``, otherwise return ``s``
     """
     if isinstance(s, text_type):
         return s.encode(encoding, errors)
-    return str(s)
+    return s
 
 
 def unicode_(s, encoding='utf-8', err='strict'):
@@ -40,4 +42,71 @@ def unicode_(s, encoding='utf-8', err='strict'):
     """
     s = s.decode(encoding, err) if isinstance(s, bytes) else s
     return _unicode(s) if s is not None else None
-to_unicode = unicode_
+text_ = to_unicode = unicode_
+
+
+if PY3:
+    def ascii_native_(s):
+        if isinstance(s, text_type):
+            s = s.encode('ascii')
+        return str(s, 'ascii', 'strict')
+else:
+    def ascii_native_(s):
+        if isinstance(s, text_type):
+            s = s.encode('ascii')
+        return str(s)
+
+
+if PY3:
+    def native_(s, encoding='latin-1', errors='strict'):
+        """ 
+        If ``s`` is an instance of ``text_type``, return
+        ``s``, otherwise return ``str(s, encoding, errors)``
+        """
+        if isinstance(s, text_type):
+            return s
+        return str(s, encoding, errors)
+else:
+    def native_(s, encoding='latin-1', errors='strict'):
+        """ 
+        If ``s`` is an instance of ``text_type``, return
+        ``s.encode(encoding, errors)``, otherwise return ``str(s)``
+        """
+        if isinstance(s, text_type):
+            return s.encode(encoding, errors)
+        return str(s)
+
+
+if PY3:
+    im_func = '__func__'
+    im_self = '__self__'
+else:
+    im_func = 'im_func'
+    im_self = 'im_self'
+
+def is_bound_method(ob):
+    return inspect.ismethod(ob) and getattr(ob, im_self, None) is not None
+
+
+try:
+    from inspect import getfullargspec as getargspec
+except ImportError:
+    from inspect import getargspec
+
+def is_unbound_method(fn):
+    """
+    This consistently verifies that the callable is bound to a
+    class.
+    """
+    is_bound = is_bound_method(fn)
+
+    if not is_bound and inspect.isroutine(fn):
+        spec = getargspec(fn)
+        has_self = len(spec.args) > 0 and spec.args[0] == 'self'
+
+        if PY2 and inspect.ismethod(fn):
+            return True
+        elif inspect.isfunction(fn) and has_self:
+            return True
+
+    return False
