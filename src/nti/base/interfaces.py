dba0e2ff9e70cce4e0ab28097ca5e03d5afa6c23
csanchez <csanchez@10.50.43.188>
2017-04-24 20:39:07 -0500

    add logger

--- src/nti/base/interfaces.py
+++ src/nti/base/interfaces.py
@@ -7,6 +7,8 @@
 from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
+
 from zope import interface
 
 from nti.base.schema import Number
