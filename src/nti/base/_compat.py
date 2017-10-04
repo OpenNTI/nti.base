99c5384edb4bca2a5ddbd9fb56487fee80a41862
Carlos Sanchez <papachoco@gmail.com>
2017-10-04 17:52:22 -0500

    remove aliases

--- src/nti/base/_compat.py
+++ src/nti/base/_compat.py
@@ -36,14 +36,14 @@ def text_(s, encoding='utf-8', err='strict'):
     """
     Return a string and unicode version of an object. 
     If the object is an byte sequence it's decoded first
-    
+
     :param object s: The object to get an unicode representation of.
     :param str encoding: The encoding to be used if ``s`` is a byte sequence
     :param str err: The err handling scheme to be used if ``s`` is a byte sequence
     """
     s = s.decode(encoding, err) if isinstance(s, bytes) else s
     return six.text_type(s) if s is not None else None
-unicode_ = to_unicode = text_
+str_ = text_  # alias
 
 
 if PY3:
